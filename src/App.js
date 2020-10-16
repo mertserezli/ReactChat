655ca14fa754279aeeaf99210348982d498940b9
Mert Serezli <mertserezli@gmail.com>
2020-10-16 12:31:18 +0300

    add authentication

--- src/App.js
+++ src/App.js
@@ -24,14 +24,42 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const [user] = useAuthState(auth);
 
-      </header>
-    </div>
+  return (
+      <div className="App">
+        <header>
+          <h1>⚛️🔥💬</h1>
+        </header>
+
+        <section>
+          {user ? <SignOut /> : <SignIn />}
+        </section>
+
+      </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+      <>
+        <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+        <p>Do not violate the community guidelines or you will be banned for life!</p>
+      </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 export default App;
